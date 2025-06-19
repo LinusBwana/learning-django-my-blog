2ba512711ad7fb79681fd5b96b20fb5838cfd35a
LinusBwana <linusbwana49@gmail.com>
2025-06-19 17:46:20 +0300

    created PostAdmin class to customize Post model display in Django admin

--- myblog/posts/admin.py
+++ myblog/posts/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "date")
+    search_fields = ("title", "body", "author__username")  # Enables search by title, body, or author's username
+    list_filter = ("author", "date")
+
+admin.site.register(Post, PostAdmin)
