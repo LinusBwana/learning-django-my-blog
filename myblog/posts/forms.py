189726733c9be6ee8fe98c302e9e2e6b41c916a8
LinusBwana <linusbwana49@gmail.com>
2025-06-19 22:54:01 +0300

    added automatic slug generation using django-autoslug

--- myblog/posts/forms.py
+++ myblog/posts/forms.py
@@ -4,4 +4,4 @@ from .models import Post
 class CreatePost(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ["title", "body", "slug", "banner"]
+        fields = ["title", "body", "banner"]
--- myblog/posts/models.py
+++ myblog/posts/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from autoslug import AutoSlugField
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=75)
     body = models.TextField()
-    slug = models.SlugField()
+    slug = AutoSlugField(populate_from='title', unique=True, max_length=200, always_update=False)
     date = models.DateTimeField(auto_now_add=True)
     banner = models.ImageField(default='fallback.png', blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
